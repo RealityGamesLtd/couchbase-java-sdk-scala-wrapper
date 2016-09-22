2d4086efb225d443051597fbb8d58cf6385e218c
Piotr Hołda <holda.piotr@gmail.com>
2016-09-22 15:33:55 +0200

    travis.yml, add stale parameter in tests

--- src/test/scala/com/realitygames/couchbase/AsyncBucketViewQueryTest.scala
+++ src/test/scala/com/realitygames/couchbase/AsyncBucketViewQueryTest.scala
@@ -1,6 +1,6 @@
 package com.realitygames.couchbase
 
-import com.couchbase.client.java.view.ViewQuery
+import com.couchbase.client.java.view.{Stale, ViewQuery}
 import com.realitygames.couchbase.ViewResult.SuccessViewResult
 import com.realitygames.couchbase.models.TestStructure
 import org.scalatest.concurrent.ScalaFutures
@@ -10,7 +10,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class AsyncBucketViewQueryTest extends AsyncWordSpec with MustMatchers with BucketTesting with ScalaFutures
   with BeforeAndAfterAll with RecoverMethods with Inside {
-  val testValueId = s"example"
+  val testValueId = "example"
 
   override def bucketName: String = "viewQuery"
 
@@ -21,7 +21,7 @@ class AsyncBucketViewQueryTest extends AsyncWordSpec with MustMatchers with Buck
   "AsyncBucket.query" should {
     "view get_object should return User value" in {
       for {
-        result <- bucket.query[TestStructure](ViewQuery.from("testStructure", "get_object").key(testValueId).limit(1))
+        result <- bucket.query[TestStructure](ViewQuery.from("testStructure", "get_object").key(testValueId).limit(1).stale(Stale.FALSE))
       } yield {
         inside(result){
           case SuccessViewResult(documents, _) =>
@@ -31,7 +31,7 @@ class AsyncBucketViewQueryTest extends AsyncWordSpec with MustMatchers with Buck
     }
     "view get_string should return string value" in {
       for {
-        result <- bucket.query[String](ViewQuery.from("testStructure", "get_string").key(testValueId).limit(1))
+        result <- bucket.query[String](ViewQuery.from("testStructure", "get_string").key(testValueId).limit(1).stale(Stale.FALSE))
       } yield {
         inside(result){
           case SuccessViewResult(documents, _) =>
@@ -41,7 +41,7 @@ class AsyncBucketViewQueryTest extends AsyncWordSpec with MustMatchers with Buck
     }
     "view get_int should return int value" in {
       for {
-        result <- bucket.query[Int](ViewQuery.from("testStructure", "get_int").key(testValueId).limit(1))
+        result <- bucket.query[Int](ViewQuery.from("testStructure", "get_int").key(testValueId).limit(1).stale(Stale.FALSE))
       } yield {
         inside(result){
           case SuccessViewResult(documents, _) =>
@@ -51,7 +51,7 @@ class AsyncBucketViewQueryTest extends AsyncWordSpec with MustMatchers with Buck
     }
     "view get_long should return byte value" in {
       for {
-        result <- bucket.query[Long](ViewQuery.from("testStructure", "get_long").key(testValueId).limit(1))
+        result <- bucket.query[Long](ViewQuery.from("testStructure", "get_long").key(testValueId).limit(1).stale(Stale.FALSE))
       } yield {
         inside(result){
           case SuccessViewResult(documents, _) =>
@@ -61,7 +61,7 @@ class AsyncBucketViewQueryTest extends AsyncWordSpec with MustMatchers with Buck
     }
     "view get_byte should return byte value" in {
       for {
-        result <- bucket.query[Byte](ViewQuery.from("testStructure", "get_byte").key(testValueId).limit(1))
+        result <- bucket.query[Byte](ViewQuery.from("testStructure", "get_byte").key(testValueId).limit(1).stale(Stale.FALSE))
       } yield {
         inside(result){
           case SuccessViewResult(documents, _) =>
@@ -71,7 +71,7 @@ class AsyncBucketViewQueryTest extends AsyncWordSpec with MustMatchers with Buck
     }
     "view get_boolean should return boolean value" in {
       for {
-        result <- bucket.query[Boolean](ViewQuery.from("testStructure", "get_boolean").key(testValueId).limit(1))
+        result <- bucket.query[Boolean](ViewQuery.from("testStructure", "get_boolean").key(testValueId).limit(1).stale(Stale.FALSE))
       } yield {
         inside(result){
           case SuccessViewResult(documents, _) =>
@@ -81,7 +81,7 @@ class AsyncBucketViewQueryTest extends AsyncWordSpec with MustMatchers with Buck
     }
     "view get_float should return float value" in {
       for {
-        result <- bucket.query[Float](ViewQuery.from("testStructure", "get_float").key(testValueId).limit(1))
+        result <- bucket.query[Float](ViewQuery.from("testStructure", "get_float").key(testValueId).limit(1).stale(Stale.FALSE))
       } yield {
         inside(result){
           case SuccessViewResult(documents, _) =>
@@ -91,7 +91,7 @@ class AsyncBucketViewQueryTest extends AsyncWordSpec with MustMatchers with Buck
     }
     "view get_double should return double value" in {
       for {
-        result <- bucket.query[Double](ViewQuery.from("testStructure", "get_double").key(testValueId).limit(1))
+        result <- bucket.query[Double](ViewQuery.from("testStructure", "get_double").key(testValueId).limit(1).stale(Stale.FALSE))
       } yield {
         inside(result){
           case SuccessViewResult(documents, _) =>
