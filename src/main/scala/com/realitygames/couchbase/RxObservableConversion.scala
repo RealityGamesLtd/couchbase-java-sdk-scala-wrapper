b588d14332d318258f254d93e29a821a8cae5ec2
Tomasz Szymula <tehshy@gmail.com>
2016-09-22 13:08:04 +0200

    async observable to future conversion

--- src/main/scala/com/realitygames/couchbase/RxObservableConversion.scala
+++ src/main/scala/com/realitygames/couchbase/RxObservableConversion.scala
@@ -1,12 +1,15 @@
 package com.realitygames.couchbase
 
+import java.util
+
 import com.couchbase.client.java.query.AsyncN1qlQueryRow
 import com.couchbase.client.java.view.AsyncViewRow
 import play.api.libs.json.{Json, Reads}
+import rx.SingleSubscriber
 
 import scala.collection.JavaConversions.iterableAsScalaIterable
 import scala.collection.breakOut
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ExecutionContext, Future, Promise}
 object RxObservableConversion {
 
   protected[couchbase] implicit def asyncViewRow2document[T](
@@ -37,13 +40,26 @@ object RxObservableConversion {
   }
 
   implicit protected[couchbase] class ObservableConversions[T](underlying: rx.Observable[T]) {
-    //TODO
-    def asFuture(implicit ec: ExecutionContext): Future[T] = Future{ underlying.toBlocking.first() }
+
+    def asFuture(implicit ec: ExecutionContext): Future[T] = {
+      val p = Promise[T]()
+      underlying.toSingle.subscribe(new SingleSubscriber[T]{
+        override def onError(error: Throwable): Unit = p.failure(error)
+        override def onSuccess(value: T): Unit = p.success(value)
+      })
+
+      p.future
+    }
 
     def asFutureList[A](implicit unpack: T => A, ec: ExecutionContext): Future[Seq[A]] = {
-      Future {
-        underlying.toList.toBlocking.first().map(unpack)(breakOut)
-      }
+
+      val p = Promise[List[A]]()
+      underlying.toList.toSingle.subscribe(new SingleSubscriber[util.List[T]] {
+        override def onError(error: Throwable): Unit = p.failure(error)
+        override def onSuccess(value: util.List[T]): Unit = p.success(value.toList.map(unpack)(breakOut))
+      })
+
+      p.future
     }
   }
 }
